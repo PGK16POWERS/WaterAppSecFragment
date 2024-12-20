138466fa5c9de6a7f5ef03ffac4587b9f0437b87
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-12-20 17:23:32 +0200

    heavy push

--- app/src/main/java/com/example/waterappfragment/MainActivity.kt
+++ app/src/main/java/com/example/waterappfragment/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen;
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
